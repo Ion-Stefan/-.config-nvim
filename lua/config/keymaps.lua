38afce948796599416f4becf3c1fd1d9e317233d
Ion Stefan <ion.adrian.stefann@gmail.com>
2024-01-18 21:46:20 +0100

    fixed global variable names

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,16 +1,16 @@
 -- Switch to the next buffer
-function next_buffer()
+function Next_buffer()
   vim.cmd("bnext")
 end
 -- Switch to the previous buffer
-function prev_buffer()
+function Prev_buffer()
   vim.cmd("bprev")
 end
 
 -- Map tab to switch to the next buffer
-vim.api.nvim_set_keymap("n", "<Tab>", ":lua next_buffer()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<Tab>", ":lua Next_buffer()<CR>", { noremap = true, silent = true })
 -- Map shift+tab to switch to the previous buffer
-vim.api.nvim_set_keymap("n", "<S-Tab>", ":lua prev_buffer()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<S-Tab>", ":lua Prev_buffer()<CR>", { noremap = true, silent = true })
 
 -- Map ctrl+d and ctrl+u to also center the page when moving up or down
 vim.api.nvim_set_keymap("n", "<C-d>", "<C-d>:normal! zz<CR>", { noremap = true, silent = true })
